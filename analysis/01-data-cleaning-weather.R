30c3ac55c06f26fe52c003d129ff5295a75d60e0
Valerie Luethi <valerie.luethi92@gmail.com>
2025-09-03 22:54:04 +0200

    decomposition of sion data

--- analysis/01-data-cleaning-weather.R
+++ analysis/01-data-cleaning-weather.R
@@ -3,62 +3,99 @@ library(readr)
 library(dplyr)
 library(lubridate)
 library(tseries)
-library(urca)
 
-# read the datasets
-df_jungfraujoch <- read.delim("data/raw/weather_monthly_jungfraujoch.csv", sep = ";")
-df_sion <- read.delim("data/raw/weather_monthly_sion.csv", sep = ';')
-df_brig_precipitation <- read.delim("data/raw/weather_rain_monthly_brig.csv")
-
-
+# read weather datasets
+df_jungfraujoch <- read.delim("data/raw/weather_monthly_jungfraujoch.csv", header=TRUE, sep = ";")
+head(df_jungfraujoch)
+str(df_jungfraujoch)
+df_sion <- read.delim("data/raw/weather_monthly_sion.csv", header=TRUE, sep = ';')
+head(df_sion)
+str(df_sion)
 
 # convert timestamp to datetime
 df_jungfraujoch <- df_jungfraujoch %>%
-  mutate(reference_timestamp = dmy_hm(reference_timestamp),
-         year_month = floor_date(reference_timestamp, "month"))
+  mutate(reference_timestamp = dmy_hm(reference_timestamp))
+str(df_jungfraujoch)
+df_sion <- df_sion %>% 
+  mutate(reference_timestamp = dmy_hm(reference_timestamp))
+str(df_sion)
+
 # only keep columns needed for analysis
-df_jungfraujoch_cleaned <- df_jungfraujoch %>% 
-  select(station_abbr, reference_timestamp, ths200m0, th9120mv)
+df_jungfraujoch_reduced <- df_jungfraujoch %>% 
+  select(station_abbr, reference_timestamp, ths200m0)
+df_sion_reduced <- df_sion %>% 
+  select(station_abbr, reference_timestamp, ths200m0)
+
 # rename columns
-colnames(df_jungfraujoch_cleaned) <- c('station_abbr', 'reference_timestamp', 'temp_mean_C', 'temp_deviation_C')
-head(df_jungfraujoch_cleaned)
-summary(df_jungfraujoch_cleaned)
+colnames(df_jungfraujoch_reduced) <- c('station_abbr', 'reference_timestamp', 'temp_mean_C')
+colnames(df_sion_reduced) <- c('station_abbr', 'reference_timestamp', 'temp_mean_C')
+
 # check for NAs
-colSums(is.na(df_jungfraujoch_cleaned)) # no NAs
+colSums(is.na(df_jungfraujoch_reduced)) # no NAs
+colSums(is.na(df_sion_reduced)) # no NAs
 
 # create time series object
 # temp_mean monthly series
-temp_mean_ts_jungfraujoch <- ts(df_jungfraujoch_cleaned$temp_mean_C,
-                   start = c(1933, 1), frequency = 12)
-# temp_deviation monthly series
-temp_deviation_ts_jungfraujoch <- ts(df_jungfraujoch_cleaned$temp_deviation_C,
-                        start = c(1933, 1), frequency = 12)
+ts_jungfraujoch <- ts(df_jungfraujoch_reduced$temp_mean_C,
+                      start = c(1933, 1),
+                      end = c(1933 + (nrow(df_jungfraujoch_reduced)-1) %/% 12,
+                              (nrow(df_jungfraujoch_reduced)-1) %% 12 + 1),
+                      frequency = 12)
+str(ts_jungfraujoch)
+
+ts_sion_full <- ts(df_sion_reduced$temp_mean_C,
+              start = c(1864, 1),
+              end = c(1864 + (nrow(df_sion_reduced)-1) %/% 12,
+                      (nrow(df_sion_reduced)-1) %% 12 + 1),
+              frequency = 12)
+str(ts_sion_full)
+
+# subset of sion data to overlap with jungfrau data
+ts_sion <- window(ts_sion_full, start = c(1933, 1), end = end(ts_jungfraujoch))
+start(ts_sion)
+end(ts_sion)
+length(ts_sion)
+
+# summary statistics
+summary(ts_jungfraujoch)
+summary(ts_sion)
 
 # plots
-plot(temp_mean_ts_jungfraujoch, main = "Monthly Mean Temperature Jungfraujoch", ylab = "Temp (°C)")
-plot(temp_deviation_ts_jungfraujoch, main = "Monthly Temperature Deviation Jungfraujoch", ylab = "Temp (°C)")
+par(mfrow=c(2,1))
+plot(ts_jungfraujoch, main="Jungfraujoch Monthly Temperature", ylab="°C")
+plot(ts_sion, main="Sion Monthly Temperature", ylab="°C")
 
-# decomposition (trend, seasonality, residuals)
-temp_mean_ts_jungfraujoch_comp=decompose(temp_mean_ts_jungfraujoch)
-temp_deviation_ts_jungfraujoch_comp=decompose(temp_deviation_ts_jungfraujoch)
-plot(temp_mean_ts_jungfraujoch_comp)
-plot(temp_deviation_ts_jungfraujoch_comp)
+# overlay plot
+plot(ts_jungfraujoch, col="blue", ylab="Temperature (°C)", main="Jungfraujoch vs Sion")
+lines(ts_sion, col="red")
+legend("topright", legend=c("Jungfraujoch","Sion"), col=c("blue","red"), lty=1)
 
-# adjust for seasonality
-temp_mean_adj <- temp_mean_ts_jungfraujoch - temp_mean_ts_jungfraujoch_comp$seasonal
-temp_dev_adj  <- temp_deviation_ts_jungfraujoch - temp_deviation_ts_jungfraujoch_comp$seasonal
-plot(temp_mean_adj)
-plot(temp_dev_adj)
+# decomposition
+ts_jungfraujoch_comp = decompose(ts_jungfraujoch)
+plot(ts_jungfraujoch_comp)
+ts_sion_comp = decompose(ts_sion)
+plot(ts_sion_comp)
 
 # check stationarity
-adf.test(temp_mean_adj)
-adf.test(temp_dev_adj)
+adf.test(ts_jungfraujoch, alternative = "stationary")
+adf.test(ts_sion, alternative = "stationary")
 
-# check ACF and PACF
-acf(temp_mean_adj_diff, main = "ACF: Differenced Temp Mean")
-pacf(temp_mean_adj_diff, main = "PACF: Differenced Temp Mean")
+# plot trend
+plot(ts_jungfraujoch_comp$trend, main="Trend Jungfraujoch")
+plot(ts_sion_comp$trend, main="Trend Sion")
 
-acf(temp_dev_adj_diff, main = "ACF: Differenced Temp Deviation")
-pacf(temp_dev_adj_diff, main = "PACF: Differenced Temp Deviation")
+# acf and pacf of residuals
+residuals_jungfrau <- ts_jungfraujoch_comp$random
+residuals_clean_jungfrau <- na.omit(residuals_jungfrau)
+acf(residuals_clean_jungfrau)
+pacf(residuals_clean_jungfrau)
+
+residuals_sion <- ts_sion_comp$random
+residuals_clean_sion <- na.omit(residuals_sion)
+acf(residuals_clean_sion)
+pacf(residuals_clean_sion)
+
+# pacf indicates seasonality
+
 
 
